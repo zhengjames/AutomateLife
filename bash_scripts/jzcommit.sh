533c94c69fabd73b4b06e5b6f9dcbc06df6f03db
zhengjames <jameszheng66@gmail.com>
2018-11-20 15:26:03 -0800

    Update jzcommit.sh

--- bash_scripts/jzcommit.sh
+++ bash_scripts/jzcommit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #mac users please put this in /usr/local/bin
 #prepend ticket name to commit message
-#ticket name is (LMS|CR)-[0-9]*
+#ticket name is [A-Z]*-[0-9]*
+#given your branch name is LMS-12345 you can do jzcommit.sh this is a commit message
+#the resulting commit message will become LMS-12345 this is a commit message
 currentBranchName=`git rev-parse --abbrev-ref HEAD`
 if (($# == 0)); then 
 	echo "Commit message cannot be empty"
